537def55f554c43403c112c89267e47fae2aeeb1
Francis <francis@francisoguesmbp.twistimage.net>
2013-11-06 15:56:19 -0500

    Updating with the latest h5bp and added my DOM based script execution

--- js/main.js
+++ js/main.js
@@ -1,20 +1,89 @@
-/* ===========================================================================
-Variables
-============================================================================== */
+( function( $, w, undefined ){
+
+	"use strict";
+
+	var MYAPP = window.MYAPP = window.MYAPP || {};
+
+	MYAPP.UTIL = {
+		exec: function( controller, action ) {
+			var ns = MYAPP,
+			action = ( action === undefined ) ? 'init' : action;
+
+			if ( controller !== '' && ns[ controller ] && typeof ns[ controller ][ action ] == 'function' ) {
+				ns[controller][action]();
+			}
+		},
+		init: function() {
+			var el = document.getElementById( 'myapp' ),
+			controller = el.getAttribute( 'data-controller' ),
+			actions = el.getAttribute( 'data-action' );
+
+			MYAPP.UTIL.exec( 'common' );
+			MYAPP.UTIL.exec( controller );
+
+			// do all the actions too.
+			$.each( actions.split( /\s+/ ),function( i, action ){
+				MYAPP.UTIL.exec( controller, action );
+			} );
+		}
+	};
 
 
-/* ===========================================================================
-jQuery Document Ready
-============================================================================== */
-$(function(){
+	// Common to the whole app/site
+	MYAPP.common = ( function(){
+		var moduleVariable = '';
+		var privateFunction = function(){};
+		var init = function(){ console.log( 'common.init' ); };
 
-});
+		return {
+			init: init
+		}
+	} )();
 
 
-/* ===========================================================================
-Function test
-More details about the function, arguments, what it returns, etc
-============================================================================== */
-function test() {
+	// Example module for the homepage (index.html)
+	MYAPP.module = ( function(){
+		var moduleVariable = '';
+		var privateFunction = function(){};
+		var init = function(){ console.log( 'module.init' ); };
+		var action1 = function(){ console.log( 'module.action1' ); };
+		var action2 = function(){ console.log( 'module.action2' ); };
 
-}
+		return {
+			init: init,
+			action1: action1,
+			action2: action2
+		}
+	} )();
+
+
+	// Example module for page1 (page1.html)
+	// MYAPP.page1 = ( function(){
+	// 	var moduleVariable = '';
+	// 	var privateFunction = function(){};
+	// 	var init = function(){ console.log( 'page1.init' ); };
+	// 	var action1 = function(){ console.log( 'page1.action1' ); };
+	// 	var action2 = function(){ console.log( 'page1.action2' ); };
+
+	// 	return {
+	// 		init: init,
+	// 		action1: action1,
+	// 		action2: action2
+	// 	}
+	// } )();
+
+
+	// Example module for page2 (page2.html)
+	// MYAPP.page2 = ( function(){
+	// 	var moduleVariable = '';
+	// 	var privateFunction = function(){};
+	// 	var init = function(){ console.log( 'page2.init' ); };
+
+	// 	return {
+	// 		init: init
+	// 	}
+	// } )();
+
+} )( jQuery, window );
+
+$( document ).ready( MYAPP.UTIL.init );
